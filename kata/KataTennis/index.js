3be4ff613676d4c68aa08c2106945d0853177485
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-10-04 07:47:57 +0700

    finish kata tennis

--- kata/KataTennis/index.js
+++ kata/KataTennis/index.js
@@ -1,3 +1,101 @@
-export function play(){
-  return true;
+import * as prompt from 'prompt';
+
+const [PLAYER1, PLAYER2] = [1, 2];
+
+function tennisGame(){
+	return {
+		score: {
+			player1: 0,
+			player2: 0
+		},
+		isEnd: false,
+		isAdv: false,
+		isDeuce: false,
+		winner: null,
+		checkGameEnd() {
+			let player1 = this.score.player1;
+			let player2 = this.score.player2;
+			if(player1 > 3 || player2 > 3) {
+				if(player1 - player2 >= 2) {
+					[this.isEnd, this.winner] = [true, PLAYER1];
+				};
+				if(player2 - player1 >= 2) {
+					[this.isEnd, this.winner] = [true, PLAYER2];
+				};
+			};
+		},
+		scoredBy(player) {
+			if(this.isAdv) {
+				// win after adv
+				if(player === PLAYER1 && this.score.player1 === 4)
+					this.score.player1 = this.score.player1 + 1;
+				if(player === PLAYER2 && this.score.player2 === 4)
+					this.score.player2 = this.score.player2 + 1;
+				// back to deuce
+				if(player === PLAYER1 && this.score.player2 === 4)
+					this.score.player2 = this.score.player2 - 1;
+				if(player === PLAYER2 && this.score.player1 === 4)
+					this.score.player1 = this.score.player1 - 1;
+				this.isAdv = false;
+				this.isDeuce = true;
+			}
+			else {
+				if(player === PLAYER1)
+					this.score.player1 = this.score.player1 + 1;
+				else if(player === PLAYER2)
+					this.score.player2 = this.score.player2 + 1;
+
+				if(this.isDeuce) {
+					this.isAdv = true;
+					this.isDeuce = false;
+				}
+				// both score reach 40
+				if(this.score.player1 === 3 && this.score.player2 === 3)
+					this.isDeuce = true;
+			};
+			this.checkGameEnd();
+		},
+		printGameState() {
+			const scoreSequence = ['love', 'fifteen', 'thirty', 'fourty'];
+			let player1 = this.score.player1;
+			let player2 = this.score.player2;
+			if(this.isEnd){
+				let player = 'PLAYER1';
+				if(player2 > player1)
+					player = 'PLAYER2';
+				console.log(`========= ${player} WIN! =========`);
+				return;
+			}
+			if(this.isDeuce) {
+				console.log('========= DEUCE =========');
+				return;
+			}
+			if(this.isAdv) {
+				let player = 'PLAYER1';
+				if(player2 > player1)
+					player = 'PLAYER2';
+				console.log(`========= ADVANTAGE ${player} =========`);
+				return;
+			}
+			console.log(`=== ${scoreSequence[player1]} : ${scoreSequence[player2]} ===`);
+		}
+	};
+}
+
+let game = tennisGame();
+
+prompt.start();
+
+getInput(game);
+
+function getInput(game){
+  prompt.get(['scored_player'], function (err, result) {
+  	let scoredPlayer = parseInt(result.scored_player);
+		
+		game.scoredBy(scoredPlayer);
+		game.printGameState();
+
+		if(game.isEnd) return;
+    getInput(game);
+  });
 }
--- kata/KataTennis/test/tennis_test.js
+++ kata/KataTennis/test/tennis_test.js
@@ -1,8 +1,122 @@
 import { assert } from 'chai';
-import { play } from '../index.js';
+import * as tennis from '../index.js';
 
-describe('play!', () => {
-  it('should return true', () => {
-    assert.equal(play(), true, 'play() return true');  	
-  })
+const [ DEUCE, PLAYER_ONE_ADV, PLAYER_TWO_ADV ] = [ 0, 1, 2 ];
+const [ PLAYER_ONE_WIN, PLAYER_TWO_WIN ] = [ 3, 4 ];
+const [ PLAYER_ONE, PLAYER_TWO ] = [ PLAYER_ONE_ADV, PLAYER_TWO_ADV ];
+
+function isScoreStateEqual(stateOne, stateTwo){
+	let isPlayerOneScoreEqual = stateOne.playerOne === stateTwo.playerOne;
+	let isPlayerTwoScoreEqual = stateOne.playerTwo === stateTwo.playerTwo;
+	return isPlayerOneScoreEqual && isPlayerTwoScoreEqual;
+}
+
+describe('update game state', () => {
+	it('should increase winner score if not deuce', () => {
+		assert.equal(
+			isScoreStateEqual(
+				tennis.updateGameState({playerOne: 0, playerTwo: 0}, PLAYER_ONE
+			), {playerOne: 15, playerTwo: 0}), true
+		);
+		assert.equal(
+			isScoreStateEqual(
+				tennis.updateGameState({playerOne: 15, playerTwo: 15}, PLAYER_ONE
+			), {playerOne: 30, playerTwo: 15}), true
+		);
+		assert.equal(
+			isScoreStateEqual(
+				tennis.updateGameState({playerOne: 30, playerTwo: 15}, PLAYER_ONE
+			), {playerOne: 40, playerTwo: 15}), true
+		);
+		assert.equal(
+			isScoreStateEqual(
+				tennis.updateGameState({playerOne: 30, playerTwo: 15}, PLAYER_TWO
+			), {playerOne: 30, playerTwo: 30}), true
+		);
+		assert.equal(
+			isScoreStateEqual(
+				tennis.updateGameState({playerOne: 15, playerTwo: 30}, PLAYER_TWO
+			), {playerOne: 15, playerTwo: 40}), true
+		);
+	});
+
+	it('should deuce if at 40/40', () => {
+		assert.equal(
+			tennis.updateGameState({playerOne: 30, playerTwo: 40}, PLAYER_ONE),
+			DEUCE
+		);
+		assert.equal(
+			tennis.updateGameState({playerOne: 40, playerTwo: 30}, PLAYER_TWO),
+			DEUCE
+		);
+	});
+
+	it('should return winner advantage after deuce', () => {
+		assert.equal(tennis.updateGameState(DEUCE, PLAYER_ONE), PLAYER_ONE_ADV);
+		assert.equal(tennis.updateGameState(DEUCE, PLAYER_TWO), PLAYER_TWO_ADV);
+	});
+
+	it('should return game winner', () => {
+		assert.equal(tennis.updateGameState({playerOne: 0, playerTwo: 40}, PLAYER_TWO), PLAYER_TWO_WIN);
+		assert.equal(tennis.updateGameState({playerOne: 15, playerTwo: 40}, PLAYER_TWO), PLAYER_TWO_WIN);
+		assert.equal(tennis.updateGameState({playerOne: 30, playerTwo: 40}, PLAYER_TWO), PLAYER_TWO_WIN);
+		assert.equal(tennis.updateGameState({playerOne: 40, playerTwo: 0}, PLAYER_ONE), PLAYER_ONE_WIN);
+		assert.equal(tennis.updateGameState({playerOne: 40, playerTwo: 15}, PLAYER_ONE), PLAYER_ONE_WIN);
+		assert.equal(tennis.updateGameState({playerOne: 40, playerTwo: 30}, PLAYER_ONE), PLAYER_ONE_WIN);
+		assert.equal(tennis.updateGameState(PLAYER_ONE_ADV, PLAYER_ONE), PLAYER_ONE_WIN);
+		assert.equal(tennis.updateGameState(PLAYER_TWO_ADV, PLAYER_TWO), PLAYER_TWO_WIN);
+	});
+});
+
+describe('get winner', () => {
+	it('should return the player which win after advantage', () => {
+    assert.equal(tennis.getWinner(PLAYER_ONE, PLAYER_ONE_ADV), PLAYER_ONE_WIN);
+    assert.equal(tennis.getWinner(PLAYER_TWO, PLAYER_TWO_ADV), PLAYER_TWO_WIN);
+  });
+
+  it('should return the player that win after his/her 40', () => {
+    assert.equal(tennis.getWinner(PLAYER_ONE, {playerOne: 40, playerTwo: 30}), PLAYER_ONE_WIN);
+    assert.equal(tennis.getWinner(PLAYER_TWO, {playerOne: 10, playerTwo: 40}), PLAYER_TWO_WIN);
+  });
+
+  it('should return null if nobody is at 40 and no advantage', () => {
+  	assert.equal(tennis.getWinner(PLAYER_TWO, {playerOne: 10, playerTwo: 30}), null);
+  	assert.equal(tennis.getWinner(PLAYER_ONE, {playerOne: 10, playerTwo: 30}), null);
+  });
+});
+
+describe('increase score', () => {
+  it('should return love when it has no argument', () => {
+    assert.equal(tennis.increaseScore(), 0);
+  });
+
+  it('should increase from 0 to 15', () => {
+    assert.equal(tennis.increaseScore(0), 15);
+  });
+
+  it('should increase from 15 to 30', () => {
+    assert.equal(tennis.increaseScore(15), 30);
+  });
+
+  it('should increase from 30 to 40', () => {
+    assert.equal(tennis.increaseScore(30), 40);
+  });
+});
+
+describe('get advantage', () => {
+  it('should set player one to be advantage if deuce', () => {
+    assert.equal(tennis.getAdvantage(DEUCE, PLAYER_ONE), PLAYER_ONE);
+  });
+
+  it('should set player two to be advantage if deuce', () => {
+    assert.equal(tennis.getAdvantage(DEUCE, PLAYER_TWO), PLAYER_TWO);
+  });
+
+  it('should deuce if player one is advantage and player two win', () => {
+    assert.equal(tennis.getAdvantage(PLAYER_ONE_ADV, PLAYER_TWO), DEUCE);
+  });
+
+  it('should deuce if player two is advantage and player one win', () => {
+    assert.equal(tennis.getAdvantage(PLAYER_TWO_ADV, PLAYER_ONE), DEUCE);
+  });
 });
